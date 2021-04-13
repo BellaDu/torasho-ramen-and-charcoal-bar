5d5587a1715a8016660069b897e76cc193a9ede9
BellaDu <ranxi@ad.unc.edu>
2021-04-14 00:24:09 +0800

    go back button

--- scripts.java/navbar.js
+++ scripts.java/navbar.js
@@ -33,13 +33,13 @@ function showSlides() {
 }
 
 /* read more */
-function myFunction() {
+  function myFunction() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
   
     if (dots.style.display === "none") {
-      dot.style.display = "inline";
+      dots.style.display = "inline";
       btnText.innerHTML = "Read more";
       moreText.style.display = "none";
     } else {
